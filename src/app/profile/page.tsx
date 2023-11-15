9d8c23c60b0225df22c5ee60e8dc74f850d236a7
Sajid <Sajidmonowar11@gmail.com>
2023-11-14 20:58:11 -0500

    Home page basics. Header setup
    Co-authored-by: theonememers2@gmail.com <theonememers2@gmail.com>
    
    zarif.ahanaf08@gmail.com
    <zarif.ahanaf08@gmail.com>

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../page"
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+      <main>
+        <Navbar></Navbar>
+        <h1>Profile</h1>
+        <footer className="">Made by Z, A, and S.</footer>
+      </main>
+    )
+  } 
