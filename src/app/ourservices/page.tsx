e1dc8899238f681a4fdda15a4d0fc362e21b6041
Sajid <Sajidmonowar11@gmail.com>
2023-11-15 20:32:33 -0500

    Idk

---
+++ src/app/ourservices/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../page"
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+      <main>
+        <Navbar></Navbar>
+        <h1>Our Services</h1>
+        <h2>Making College Application come to you</h2>
+        <footer className="">Made by Z, A, and S.</footer>
+      </main>
+    )
+  } 
