4dce1d7ed495d9babd35b444c032686a4a74899a
Sajid <Sajidmonowar11@gmail.com>
2024-09-21 14:41:36 -0400

    Co-authored-by: theonememers2@gmail.com <theonememers2@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    experimental: {
+    }
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ let title = "Grapes and Bananas 🍇🍌"
 
 export function Navbar() {
   return(
-    <header className='bg-white flex justify-between items-center w-full h-20 text-black sticky nav px-6'>
+    <header className='bg-white flex justify-between items-center w-full min-h-[5rem] text-black sticky nav px-6 flex-wrap'>
       <Link href="/"><h1 className="text-2xl">{title}</h1></Link>
       <div id="links" className="flex gap-3">
         <Link href="/aboutus"><h3>About Us</h3></Link>
         <Link href="/ourservices"><h3>Our Services</h3></Link>
-        <Link href="/profile"><h3>Profile</h3></Link>
+        <Link href="/login"><h3>Profile</h3></Link>
       </div>
     </header>
   )
@@ -21,7 +21,6 @@ export default function Home() {
     <main>
       <Navbar></Navbar>
       <h1>Hello</h1>
-      <footer>Made by Z, A, and S.</footer>
     </main>
   )
 } 
